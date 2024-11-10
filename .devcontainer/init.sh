20d182b1c91cfe200c0a9bf24e7325b9ffb643a9
Antón Seoane Ampudia <anton.seoane-ampudia.8277@student.uu.se>
2024-11-10 16:23:39 +0100

    Devcontainer configuration

---
+++ .devcontainer/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_PATH=`dirname $0 | while read a; do cd $a && pwd && break; done`
+echo $SCRIPT_PATH
+cd ${SCRIPT_PATH}/..
+
+bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-native-debug-symbols=internal --disable-warnings-as-errors
+bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-debug-level=slowdebug --with-native-debug-symbols=internal --disable-warnings-as-errors
+make CONF=rel compile-commands
+make CONF=rel
+make CONF=slow
+exit
