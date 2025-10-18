c386a8d7d6970d0e84d7458ed7b6df14d9384763
aseoane-uu <anton.seoane-ampudia.8277@student.uu.se>
2025-10-18 20:11:38 +0200

    Googletest config

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,8 +5,8 @@ SCRIPT_PATH=`dirname $0 | while read a; do cd $a && pwd && break; done`
 echo $SCRIPT_PATH
 cd ${SCRIPT_PATH}/..
 
-bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-native-debug-symbols=internal --disable-warnings-as-errors
-bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-debug-level=slowdebug --with-native-debug-symbols=internal --disable-warnings-as-errors
+bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-native-debug-symbols=internal --disable-warnings-as-errors --with-gtest=/workspaces/jdk-uplang/googletest
+bash configure --with-boot-jdk=/home/vscode/jdk-23.0.1/ --with-debug-level=slowdebug --with-native-debug-symbols=internal --disable-warnings-as-errors --with-gtest=/workspaces/jdk-uplang/googletest
 make CONF=rel compile-commands
 make CONF=rel
 make CONF=slow
